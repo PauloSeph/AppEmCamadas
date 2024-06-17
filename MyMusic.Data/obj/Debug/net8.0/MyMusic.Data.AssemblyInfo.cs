15bf3a4de6773e1019cc972a8b240d7c12cd4cc4
PauloSeph <paulo.seph@hotmail.com>
2024-06-17 16:37:56 -0300

    Atualizando projeto

--- MyMusic.Data/obj/Debug/net8.0/MyMusic.Data.AssemblyInfo.cs
+++ MyMusic.Data/obj/Debug/net8.0/MyMusic.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMusic.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21ea6fb9f64d2fcc962662d36e0c09be60043770")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ad4932786a0eb259e5b66a06c11592dbf03e890")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMusic.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMusic.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
